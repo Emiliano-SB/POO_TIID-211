ec814066f5978bc0d2375c62fac4670a62783580
Emiliano-SB <124051706@upq.edu.mx>
2025-05-13 08:49:47 -0600

    Se creo un publisher

---
+++ Project_02/Project_02/src/Variables.java
@@ -0,0 +1,26 @@
+public class Variables 
+{
+    public static void main(String[] args) 
+    {
+        int numero1 = 10;
+        double numero2;
+        numero2 = 3.14; //Puede o no colocar una D o d al final del número
+        float numero3;
+        numero3 = 3.14159F; //Para que no se confunda que es un double se pone una F para que se convierta a Float
+        char caracter1 = 'A', caracter2 = 65;
+        boolean opcion = true;
+        long numero4; //Puede requerir L o l al final del número
+        numero4 = 987654321L;
+        byte numero5 = 126;
+        short numero6 = 128;
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+        System.out.println(numero5);
+        System.out.println(numero6);
+        System.out.println(caracter1);
+        System.out.println(caracter2);
+        System.out.println(opcion);
+    }
+}
