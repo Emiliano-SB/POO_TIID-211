0722d189fa2167351d5c9b7f64b0033d08e9b6fd
Emiliano-SB <124051706@upq.edu.mx>
2025-05-13 10:52:53 -0600

    Practica de variables en JAVA

--- Project_02/Project_02/src/Variables.java
+++ Project_02/Project_02/src/Variables.java
@@ -22,5 +22,37 @@ public class Variables
         System.out.println(caracter1);
         System.out.println(caracter2);
         System.out.println(opcion);
+
+        //Variables de referencia 
+        String mensaje, mensaje2;
+        mensaje = "Esto es un mensaje";
+        mensaje2 = """ 
+                Este
+                es 
+                un mensaje
+                multilinea
+                """; //Este tipo de mensaje se debe de poner si o si las comillas en una linea sola 
+
+        System.out.println(mensaje);
+        System.out.println(mensaje2); 
+        System.out.println(mensaje + ' ' + numero1); //Para concatenar las cosas y poner espacios debe de ser comillas simples y el espacio
+        System.out.println(mensaje.toUpperCase());
+        
+        String numero1_string = Integer.toString(numero1); //Convertir enteros a caracteres 
+        System.out.println(mensaje.concat(numero1_string));
+
+        //Variable general
+
+        var variable1 = 15;
+        var variable2 = 'F';
+        var variable3 = 2.8754F;
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+
+        //Constantes
+        final var PI = 3.14159; //Para declarar constantes como lo puede ser PI o otras cosas
+        //PI=3.14159 Este NO funciona
+        System.out.println(PI);
     }
 }
